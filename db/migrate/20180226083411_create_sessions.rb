777b5202795facdff9fc54dc1c2879f7cab2183e
Zachary Gian <z.gian12@eabjm.org>
2018-02-26 18:00:01 +0900

    created models

---
+++ db/migrate/20180226083411_create_sessions.rb
@@ -0,0 +1,11 @@
+class CreateSessions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :sessions do |t|
+      t.date :date
+      t.time :time
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
