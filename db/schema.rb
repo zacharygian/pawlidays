391f5beb080d5aad67ef499859a9d414ea9ec620
Nozomi Okuma <skyidentity@gmail.com>
2018-03-01 12:16:17 +0900

    added configuration for google maps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228044657) do
+ActiveRecord::Schema.define(version: 20180301031138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20180228044657) do
     t.string "animal_type"
     t.string "animal_size"
     t.boolean "onsite"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
