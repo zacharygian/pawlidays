0ba1af0cc987098d94fcb16b35146aa0d3ba31b4
Zachary Gian <z.gian12@eabjm.org>
2018-03-01 15:30:35 +0900

    finished migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301031138) do
+ActiveRecord::Schema.define(version: 20180301060934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180301031138) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "availability", default: [], array: true
+    t.text "description"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
